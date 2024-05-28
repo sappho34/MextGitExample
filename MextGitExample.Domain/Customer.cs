0518530daf06f7374b94439487642d35f7e2d232
sappho34 <110392965+sappho34@users.noreply.github.com>
2024-05-28 14:08:43 +0300

    Added Customer class

---
+++ MextGitExample.Domain/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MextGitExample.Domain
+{
+    public class Customer
+    {
+        private Guid Id {  get; set; }
+        private string Name {  get; set; }
+        private string  PhoneNumber { get; set; }
+        private string City { get; set; }
+
+
+    }
+}
