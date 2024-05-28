0b1dade447df6773038662f7109ac533bc32897d
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2024-05-28 13:39:22 +0300

    Update Product.cs
    Quantity added

--- MextGitExample.Domain/Product.cs
+++ MextGitExample.Domain/Product.cs
@@ -11,5 +11,6 @@ namespace MextGitExample.Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
